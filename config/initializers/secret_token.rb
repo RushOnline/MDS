e1a1415e2b749adecbae64ebd749dc99bc6dfac2
Evgeni (Rush) Nepomniashchi <rush.zlo@gmail.com>
2012-03-19 15:52:08 +0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MDS::Application.config.secret_token = 'd5d9e8ccb068aa69fc4b3ac93a3212b2f6835828315fbfc205ae028e6907593240f4379638c5fbca0a16c88845683aaa5b118bc3897d7d06d1554c62ba7ab67f'
