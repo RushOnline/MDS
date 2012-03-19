92e81e04f9b5b36c5cccac6b93d2b7b81629816f
Evgeni (Rush) Nepomniashchi <rush.zlo@gmail.com>
2012-03-19 20:20:13 +0400

    add relations to model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    has_many :mds_file
+
+    validates :name, :length => { :maximum => 140 }
 end
--- app/models/mds_file.rb
+++ app/models/mds_file.rb
@@ -1,2 +1,5 @@
 class MdsFile < ActiveRecord::Base
+    belongs_to :author
+
+    validates :title, :length => { :maximum => 250 }
 end
