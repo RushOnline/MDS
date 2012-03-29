8e246b8b58682ba18c7bf7c5109a9eebb23bddb9
Evgeni (Rush) Nepomniashchi <rush.zlo@gmail.com>
2012-03-29 11:06:10 +0400

    player component removed

--- app/components/mds_app.rb
+++ app/components/mds_app.rb
@@ -1,11 +1,12 @@
 class MdsApp < Netzke::Basepack::SimpleApp
 
+=begin
     def menu
         [:podcast_grid.action, :player.action] + super
     end
-
+=end
     action :podcast_grid, :icon => :table, :handler => :load_netzke_component_by_action
-    action :player, :handler => :load_netzke_component_by_action
+#    action :player, :handler => :load_netzke_component_by_action
 
     component :podcast_grid
 end
